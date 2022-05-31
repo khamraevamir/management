edc8f0bb1b13e20cfbf7a2b340c4eb34163ec581
khamraevamir <khamraevamir@gmail.com>
2022-05-31 13:20:33 +0500

    ok

--- demo/back/settings.py
+++ demo/back/settings.py
@@ -118,8 +118,11 @@ import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
